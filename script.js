49e04864c7cca469ccab4e5e0e08812fb8843919
yelim03 <yelim5570@gmail.com>
2023-12-10 16:17:28 +0900

    231210

--- script.js
+++ script.js
@@ -6,58 +6,9 @@ Nav_btn.addEventListener("click", () => {
   Body.classList.toggle("nav_active");
 });
 
-const sec0 = () => {
-  const $text = document.querySelector(".typing .text");
-
-  // 글자 모음
-  const letters = ["Developer", "Designer", "Publisher"];
-
-  // 글자 입력 속도
-  const speed = 100;
-  let i = 0;
-
-  // 타이핑 효과
-  const typing = async () => {
-    const letter = letters[i].split("");
-
-    while (letter.length) {
-      await wait(speed);
-      $text.innerHTML += letter.shift();
-    }
-
-    // 잠시 대기
-    await wait(800);
-
-    // 지우는 효과
-    remove();
-  };
-
-  // 글자 지우는 효과
-  const remove = async () => {
-    const letter = letters[i].split("");
-
-    while (letter.length) {
-      await wait(speed);
-
-      letter.pop();
-      $text.innerHTML = letter.join("");
-    }
-
-    // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
-    i = !letters[i + 1] ? 0 : i + 1;
-    typing();
-  };
-
-  // 딜레이 기능 ( 마이크로초 )
-  function wait(ms) {
-    return new Promise((res) => setTimeout(res, ms));
-  }
-
-  // 초기 실행
-  setTimeout(typing, 1500);
-};
+const sec0 = () => {};
 const sec1 = () => {
-  console.log("sec1");
+  
 };
 const sec2 = () => {
   console.log("sec2");
